3e33f834436bd9789c1a667f57ef28785dced797
WilliamBork33 <williambork33@gmail.com>
2018-11-14 11:02:46 -0600

    Setting up Environment Variables

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Telos2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Code needed for local_env.yml to provide environment variables so I can .gitignore them.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
