674360d0b68e23d237c48432d92b0b3523676ffc
WilliamBork33 <williambork33@gmail.com>
2018-11-15 10:02:40 -0600

    Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new_user'
   #root 'welcome#index'
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
+  get  '/signup',  to: 'users#new_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
