09073d8593e0cc9afbb833f1376747e225e7b942
WilliamBork33 <williambork33@gmail.com>
2018-11-17 13:33:20 -0600

    Generated controller, model, and migrated db for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'books/new'
   get 'sessions/new'
   #root 'welcome#index'
   root 'static_pages#home'
---
+++ db/migrate/20181117183113_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :description
+      t.string :author
+      t.string :seller
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
