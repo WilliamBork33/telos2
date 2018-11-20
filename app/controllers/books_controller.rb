4ec59a74fd4a7945971b1e65be5a7f450e9e3955
WilliamBork33 <williambork33@gmail.com>
2018-11-19 18:09:56 -0600

    Book controller actions revision

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,41 +1,22 @@
 class BooksController < ApplicationController
   
   def index
-    @a = User.find([35])
-    @b = User.find_by(id: '35')
-    @c = Book.find_by(id: '27')
-    #@d = User.all(params[:id])
-
-   
-    #User.find(params[:id]).destroy
-
-    #@users = User.paginate(page: params[:page])
-    #@user = User.all
-   
-    #@books = Book.all
-    #@book = Book.paginate(page: params[:page])
-
-    #@bookss = User.all
-    #@testbook = Book.last.user_id
-    #@testbook1 = Book.first.title
-    #@testbook2 = Book.exists?(id: [1, 4, 8])
-
+    #@user_id = User.all
+    @book_list = Book.all
   end
 
   def show
-    
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @book = Book.find(params[:id])
+    @user_id = current_user.name
   end
   
   def new
     @book = Book.new
-   
   end
 
   def create
-   
-    @book = Book.new(book_params)    # Not the final implementation!
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to @book
@@ -44,22 +25,6 @@ class BooksController < ApplicationController
     end
   end
 end
-
-  #def show
-    #@books = Book.all
-    #respond_with(@books)
-    #user = User.first
-   #@books = books.first(:id)
-    #@books = current_user.books.build(books_params)
-
-    #@user = User.find(params[:id])
-    #@book1 = user.book.find(params[:id])
-    #@book2 = user.books.find(params[:id])
-    #@books3 = user.book.find(params[:id])
-    #@books4 = user.books.find(params[:id])
-
-
-  #end
 
   private
 
