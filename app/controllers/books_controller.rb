7c40fe1d83db0f0d0fd908a6fe4d724045c8c70a
WilliamBork33 <williambork33@gmail.com>
2018-11-20 23:42:55 -0600

    Styling

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,10 @@
 class BooksController < ApplicationController
   
   def index
-    #@user_id = User.all
     @book_list = Book.all
-    #@user_id = current_user.name
   end
 
   def show
-    @user = User.find(params[:id])
     @book = Book.find(params[:id])
     @user_id = current_user.name
   end
@@ -26,23 +23,19 @@ class BooksController < ApplicationController
     end
   end
 
-
   def destroy
     Book.find(params[:id]).destroy
     flash[:success] = "Book Removed from Market"
     redirect_back(fallback_location: users_url)
   end
 
-end
-
   private
 
-
-  def user_params
-    params.require(:user).permit(:name, :email, :address, :phone, :password, :password_confirmation)
-  end
-   
+    def user_params
+      params.require(:user).permit(:name, :email, :address, :phone, :password, :password_confirmation)
+    end
 
     def book_params
       params.require(:book).permit(:title, :description, :author, :price, :user_id)
     end
+end
